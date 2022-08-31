83f95538215d51b921b416a180db0e0ade8dda2b
Weslley de Oliveira <weslley@voxtec.com.br>
2022-08-31 15:31:13 -0300

    Refatorando o código do objeto Calculadora

--- src/js/calculadora.js
+++ src/js/calculadora.js
@@ -1,35 +1,55 @@
 let Calculadora = {
+
+    ADICAO: '+',
+    SUBTRACAO: '-',
+    DIVISAO: '/',
+    MULTIPLICACAO: '*',
+
     adicionar: function(num1 , num2) {
-
-        if(isNaN(num1) || isNaN(num2)) {
-            return 0;
-        }
-
-        num1 = parseFloat(num1);
-        num2 = parseFloat(num2);
-        return num1 + num2;
+        return Calculadora.calcular(num1, num2, Calculadora.ADICAO);
     },
     subtrair: function(num1 , num2) {
-        if (isNaN(num1) || isNaN(num2)) {
-            return 0;
-        }
-        return num1 - num2;
+        return Calculadora.calcular(num1, num2, Calculadora.SUBTRACAO);
     },
     dividir: function(num1 , num2) {
         if(num2 == 0) {
             return 'Erro';
         }
+        return Calculadora.calcular(num1, num2, Calculadora.DIVISAO);
+    },
+    multiplicar: function(num1 , num2) {
+        return Calculadora.calcular(num1, num2, Calculadora.MULTIPLICACAO);
+    },
+
+    calcular: function(num1, num2, operacao) {
+        let resultado = 0;
+        num1 = parseFloat(num1);
+        num2 = parseFloat(num2);
+
         if (isNaN(num1) || isNaN(num2)) {
             return 0;
         }
-        return num1 / num2;
-    },
-    multiplicar: function(num1 , num2) {
-        if(isNaN(num1) || isNaN(num2)) {
-            return 0;
+
+        switch(operacao) {
+            case Calculadora.ADICAO:
+                resultado = num1 + num2;
+                break;
+            case Calculadora.SUBTRACAO:
+                resultado = num1 - num2;
+                break;
+            case Calculadora.DIVISAO:
+                resultado = num1 / num2;
+                break;
+            case Calculadora.MULTIPLICACAO:
+                resultado = num1 * num2;
+                break;
+            default:
+                resultado = 0;
+                break;
         }
-        return num1 * num2;
-    },
+
+        return resultado;
+    }
 };
 
 if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
